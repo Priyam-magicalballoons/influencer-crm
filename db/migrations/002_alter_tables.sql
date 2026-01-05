a035e40a1635751eebe76d1a4595fabae6487f17
B-priyam <priyam3801h@gmail.com>
2026-01-05 18:27:23 +0530

    session table created

--- db/migrations/002_alter_tables.sql
+++ db/migrations/002_alter_tables.sql
@@ -1,3 +1,11 @@
 -- ALTER TABLE influencers ALTER COLUMN brand_name TYPE VARCHAR(255);
 
-ALTER TABLE influencers ADD COLUMN creator_name VARCHAR(100);
+-- ALTER TABLE influencers ADD COLUMN creator_name VARCHAR(100);
+
+CREATE TABLE sessions(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    role ROLES NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+)
