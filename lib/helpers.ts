e3ff21ade4d1b5995daf96b3dbafe543bf071ae1
B-priyam <priyam3801h@gmail.com>
2026-01-06 12:00:45 +0530

    helpers updated for access token and refresh token

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -3,10 +3,11 @@
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 import { redirect } from "next/navigation";
+import { sql } from "@/db";
 
 export const tokenExists = async () => {
   try {
-    const token = (await cookies()).get("token");
+    const token = (await cookies()).get("access");
     if (!token) {
       return false;
     }
@@ -18,14 +19,14 @@ export const tokenExists = async () => {
 
 export const getRole = async () => {
   try {
-    const token = (await cookies()).get("token")?.value;
+    const token = (await cookies()).get("access")?.value;
     if (!token) {
       return {
         status: 400,
         role: null,
       };
     }
-    const role = jwt.decode(token)?.split("_")[1];
+    const role = jwt.decode(token) as any;
     if (!role) {
       return {
         status: 400,
@@ -35,7 +36,7 @@ export const getRole = async () => {
 
     return {
       status: 200,
-      role: role,
+      role: role.role,
     };
   } catch (error) {
     return {
@@ -45,44 +46,46 @@ export const getRole = async () => {
   }
 };
 
-export const getUserData = async () => {
-  try {
-    const token = (await cookies()).get("token")?.value;
+// export const getUserData = async () => {
+//   try {
+//     const token = (await cookies()).get("access")?.value;
 
-    if (!token) {
-      return {
-        status: 400,
-        user: null,
-      };
-    }
+//     if (!token) {
+//       return {
+//         status: 400,
+//         user: null,
+//       };
+//     }
 
-    const user = jwt.decode(token);
+//     const { id, role } = jwt.decode(token) as any;
 
-    if (!user) {
-      return {
-        status: 400,
-        user: null,
-      };
-    }
+//     if (!id) {
+//       return {
+//         status: 400,
+//         user: null,
+//       };
+//     }
 
-    const [userId, role] = user?.split("_");
-
-    return {
-      status: 200,
-      user: {
-        userId,
-        role,
-      },
-    };
-  } catch (error) {
-    return {
-      status: 400,
-      user: null,
-    };
-  }
-};
+//     return {
+//       status: 200,
+//       user: {
+//         userId: id,
+//         role,
+//       },
+//     };
+//   } catch (error) {
+//     return {
+//       status: 400,
+//       user: null,
+//     };
+//   }
+// };
 
 export const logoutUser = async () => {
-  (await cookies()).delete("token");
+  const refresh = (await cookies()).get("refresh")?.value;
+  (await cookies()).delete("access");
+  (await cookies()).delete("refresh");
+
+  await sql`DELETE FROM sessions WHERE id=${refresh}`;
   redirect("/login");
 };
